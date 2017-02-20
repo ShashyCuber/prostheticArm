fa5ccdea15604107260eeffa8de145127cb111a8
ShashyCuber <mailtoshashwatsparsh@gmail.com>
2017-02-20 09:50:25 -0800

    Added Comment
    Comment contains code

--- src/motorControl/motorControl.ino
+++ src/motorControl/motorControl.ino
@@ -1,3 +1,8 @@
+const int buttonB = 3;
+const int buttonF = 5;
+
+
+
 void setup() {
   // put your setup code here, to run once:
 
@@ -7,3 +12,35 @@ void loop() {
   // put your main code here, to run repeatedly:
 
 }
+
+
+
+
+/*
+const int buttonB = 3;
+const int buttonF = 5;
+int buttonStateb = digitalRead(buttonB);
+int buttonStateF = digitalRead(buttonF);
+
+
+if ((buttonStateb == LOW) && (buttonStatef == LOW))
+  {
+    continue;
+  }
+  else if (buttonStateb == LOW)
+  {
+    //run backward
+  }
+  else if (buttonStatef == LOW)
+  {
+    //run forward
+  }
+  */
+
+
+/* Motor Control
+ *  
+ *  
+ */
+
+
