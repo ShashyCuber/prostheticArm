64bb736c2ed2382d1417450956b8f1681e5945bc
ShashyCuber <shashwat.sparsh@gmail.com>
2017-02-24 08:20:23 -0800

    Removed LED code and replaced with Motor code

--- src/motorControl/motorControl.ino
+++ src/motorControl/motorControl.ino
@@ -1,7 +1,7 @@
 const int buttonB = 3;
 const int buttonF = 5;
 
-const int DIR_F = 10; //Set pin for Forward Direction  
+const int DIR_F = 10; //Set pin for Forward Direction
 const int DIR_B = 8; //Set pin for Backward Direction
 const int PWM = 11; // Set speed output pin
 
@@ -9,62 +9,46 @@ const int PWM = 11; // Set speed output pin
 
 void setup()
 {
- /*
- pinMode (DIR_F, OUTPUT);
- pinMode (DIR_B, OUTPUT);
- pinMode (PWM, OUTPUT);
- */
- pinMode (buttonB, INPUT);
- pinMode (buttonF, INPUT); 
- pinMode (2, OUTPUT);
- pinMode (1, OUTPUT);
- pinMode (0, OUTPUT);
+  pinMode (DIR_F, OUTPUT);
+  pinMode (DIR_B, OUTPUT);
+  pinMode (PWM, OUTPUT);
+  pinMode (buttonB, INPUT);
+  pinMode (buttonF, INPUT);
+  
 }
 
 void loop()
-{ 
- int buttonStateb = digitalRead(buttonB);
- int buttonStatef = digitalRead(buttonF);
+{
+  int buttonStateb = digitalRead(buttonB);
+  int buttonStatef = digitalRead(buttonF);
 
- if ((buttonStateb == LOW) && (buttonStatef == LOW))
- {
-  digitalWrite(2, HIGH);
-  digitalWrite(1, HIGH);
-  digitalWrite(0, HIGH);
-  /*
-  digitalWrite(DIR_F, LOW);
-  digitalWrite(DIR_B, LOW);
-  analogWrite(PWM, 0);
-  */
- }
- else if ((buttonStateb == HIGH) && (buttonStatef == HIGH))
- {
-  digitalWrite(2, LOW);
-  digitalWrite(1, LOW);
-  digitalWrite(0, LOW);
- }
- else if ((buttonStateb == LOW) && (buttonStatef == HIGH))
- {
-  digitalWrite(2, LOW);
-  digitalWrite(1, HIGH);
-  digitalWrite(0, LOW);
-  /*
-  digitalWrite(DIR_F, LOW);
-  digitalWrite(DIR_B, HIGH);
-  analogWrite(PWM, 255);
-  */
- }
- else if ((buttonStatef == LOW) && (buttonStateb == HIGH))
- {
-  digitalWrite(2, LOW);
-  digitalWrite(1, LOW);
-  digitalWrite(0, HIGH);
-  /*
-  digitalWrite(DIR_F, HIGH);
-  digitalWrite(DIR_B, LOW);
-  analogWrite(PWM, 255); 
-  */
- }
+  if ((buttonStateb == LOW) && (buttonStatef == LOW))
+  {
+   digitalWrite(DIR_F, LOW);
+   digitalWrite(DIR_B, LOW);
+   analogWrite(PWM, 0);
+  }
+  
+  else if ((buttonStateb == HIGH) && (buttonStatef == HIGH))
+  {
+   digitalWrite(DIR_F, LOW);
+   digitalWrite(DIR_B, LOW);
+   analogWrite(PWM, 0);
+  }
+  
+  else if ((buttonStateb == LOW) && (buttonStatef == HIGH))
+  {
+   digitalWrite(DIR_F, LOW);
+   digitalWrite(DIR_B, HIGH);
+   analogWrite(PWM, 255);
+  }
+  
+  else if ((buttonStatef == LOW) && (buttonStateb == HIGH))
+  {
+   digitalWrite(DIR_F, HIGH);
+   digitalWrite(DIR_B, LOW);
+   analogWrite(PWM, 255);
+  }
 
 
 }
